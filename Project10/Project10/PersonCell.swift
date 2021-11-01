491dad0bb535dae9d5d7133aa6e5316082bd86e0
Satochaaan <k97satosi327@gmail.com>
2021-11-02 08:40:29 +0900

    Project10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by 川野智史 on 2021/10/31.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
