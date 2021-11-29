62ede43d4e95709df968cc5357b5a537eb1e78d4
Satochaaan <k97satosi327@gmail.com>
2021-11-30 08:52:31 +0900

    Project13 part1

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by 川野智史 on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
