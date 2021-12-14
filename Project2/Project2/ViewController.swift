f4a2aaed8445649fb6f73c6c5fb8668b1a750ec6
Satochaaan <k97satosi327@gmail.com>
2021-12-15 08:53:05 +0900

    Day58 Project2 Challenge

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -44,12 +44,21 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        button1.transform = .identity
+        button2.transform = .identity
+        button3.transform = .identity
+        
         title = countries[correctAnswer].uppercased()
     }
     
     @IBAction func buttonTaped(_ sender: UIButton) {
         var title: String
         var message: String
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+            sender.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        }
         
         if sender.tag == correctAnswer {
             title = "Correct"
